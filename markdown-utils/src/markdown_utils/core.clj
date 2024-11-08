cdc7adae6f1c331ffafa05eb69b1de10c1383d71
kip2 <tqa1no4@gmail.com>
2024-11-08 22:05:58 +0900

    Update convert markdown checklist

--- markdown-utils/src/markdown_utils/core.clj
+++ markdown-utils/src/markdown_utils/core.clj
@@ -1,7 +1,34 @@
 (ns markdown-utils.core
-  (:gen-class))
+  (:require [clj-clip.core :as clip]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(clip/write-clip "Hello from clj-clip!")
+(println (clip/read-clip))
+
+(defn convert-to-markdown-checklist [lst]
+  (let [convert-checklist (fn [element] (str "- [ ] [[" element "(Clojure)]]\n"))]
+    (apply str (map convert-checklist lst))))
+
+(defn convert-md-checklist-with-output-txt [lst output-name]
+  (->> (convert-to-markdown-checklist lst)
+       (spit output-name)))
+
+(defn convert-md-checklist-and-copy-to-clipboard [lst]
+  (->> (convert-to-markdown-checklist lst)
+       (clip/write-clip)))
+
+
+(def convert-list ["map-indexed"
+                   "drop"
+                   "partition-by"
+                   "partirion"
+                   "flatten"
+                   "re-find"
+                   "spit"])
+
+(convert-to-markdown-checklist convert-list)
+
+(convert-md-checklist-with-output-txt convert-list "output2.txt")
+
+(convert-md-checklist-and-copy-to-clipboard convert-list)
+
+
