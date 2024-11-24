8dd18e64b0e244440b8de90c0d3672c059a4c332
kip2 <tqa1no4@gmail.com>
2024-11-24 19:08:32 +0900

    Add generate mermaid function

--- callgraph/src/callgraph/core.clj
+++ callgraph/src/callgraph/core.clj
@@ -1,7 +1,26 @@
 (ns callgraph.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]
+            [clj-clip.core :as clip]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn generate-mermaid-string [caller callee]
+  (-> (str (:fc caller) "[" (:ns caller) "/" (:fc caller) "]")
+      (str " " "-->" " ")
+      (str (:fc callee)  "[" (:ns callee) "/" (:fc callee) "]")))
+
+(defn generate-mermaid-strings [data]
+  (->> data
+       (map (fn [[caller callee]] (generate-mermaid-string caller callee)))
+       (str/join "\n")))
+
+(def data [[{:fc "run" :ns "core"} {:fc "cursor-up" :ns "tool"}]
+           [{:fc "run1" :ns "core"} {:fc "cursor-up1" :ns "tool"}]
+           [{:fc "run2" :ns "core"} {:fc "cursor-up2" :ns "tool"}]
+           [{:fc "run3" :ns "core"} {:fc "cursor-up3" :ns "tool"}]
+           [{:fc "run4" :ns "core"} {:fc "cursor-up4" :ns "tool"}]])
+
+(defn copy-mermaid-to-clipboard [data]
+  (clip/write-clip (generate-mermaid-strings data)))
+
+(copy-mermaid-to-clipboard data)
+
